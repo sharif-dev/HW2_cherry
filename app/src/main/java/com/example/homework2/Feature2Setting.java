f146cb70a53475a1151ed3690f902be759ff383c
javid <jatages@gmail.com>
2020-04-25 12:40:02 -0700

    complete part2 99.9%

--- app/src/main/java/com/example/homework2/Feature2Setting.java
+++ app/src/main/java/com/example/homework2/Feature2Setting.java
@@ -1,24 +1,105 @@
 package com.example.homework2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.Toast;
 
-public class Feature2Setting extends AppCompatActivity {
+import com.marcinmoskala.arcseekbar.ArcSeekBar;
+import com.marcinmoskala.arcseekbar.ProgressListener;
 
+import org.jetbrains.annotations.NotNull;
+
+public class Feature2Setting extends AppCompatActivity {
+    boolean serviceIsAlive =false;
+
+    ArcSeekBar  arcSeekBar ;
+    Switch feat2Switch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feature2_setting);
+        SharedPreferences sharedPrefs = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPrefs.edit();
+        feat2Switch = (Switch) findViewById(R.id.feature2Switch);
+        final ArcSeekBar arcSeekBar  = (ArcSeekBar) findViewById(R.id.seekArc);
+        serviceIsAlive = sharedPrefs.getBoolean("isAlive",false);
+        feat2Switch.setChecked(serviceIsAlive);
+        arcSeekBar.setProgress(sharedPrefs.getInt("LastLimit",100));
+        if (feat2Switch.isChecked()){
+            Intent intent = new Intent(Feature2Setting.this, ShakeEventListener.class);
+            int j = arcSeekBar.getProgress();
+            intent.putExtra("limit",j);
+            startService(intent);
+            serviceIsAlive = true;
+            editor.putBoolean("isAlive",serviceIsAlive);
+        }
 
 
-        startService(new Intent(this, ShakeEventListener.class));
+
+
+
+
+        int[] intArray = getResources().getIntArray(R.array.progressGradientColors);
+        arcSeekBar.setProgressGradient(intArray);
+        arcSeekBar.setOnProgressChangedListener(new ProgressListener() {
+            @Override
+            public void invoke(int i) {
+                if (serviceIsAlive){
+                Intent intent = new Intent(Feature2Setting.this, ShakeEventListener.class);
+                int j = (int) (i/6);
+                intent.putExtra("limit",j);
+                startService(intent);
+                }
+                editor.putInt("LastLimit",i);
+                editor.apply();
+            }
+    });
+
+        feat2Switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    int i = arcSeekBar.getProgress();
+                    Intent intent = new Intent(Feature2Setting.this, ShakeEventListener.class);
+                    int j = (int) (i/6);
+                    intent.putExtra("limit",j);
+                    startService(intent);
+                    serviceIsAlive = true;
+                    editor.putBoolean("isAlive",serviceIsAlive);
+                }else{
+                    stopService(new Intent(Feature2Setting.this, ShakeEventListener.class));
+                    serviceIsAlive = false;
+                    editor.putBoolean("isAlive",serviceIsAlive);
+                }
+                editor.apply();
+            }
+        });
+
+//        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+//        dialog.setMessage("Allow app to control your lock screens");
+//        dialog.setTitle("Sleep Mode alert");
+//        dialog.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int which) {
+//                Toast.makeText(getApplicationContext(),"hi",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        AlertDialog alertDialog = dialog.create();
+//        alertDialog.show();
 
     }
+
 }
--- app/src/main/java/com/example/homework2/ShakeEventListener.java
+++ app/src/main/java/com/example/homework2/ShakeEventListener.java
@@ -1,6 +1,7 @@
 package com.example.homework2;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.Service;
 import android.content.Context;
@@ -10,12 +11,17 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 
 public class ShakeEventListener extends Service implements SensorEventListener {
     private static int SHAKE_LIMIT = 2;
@@ -27,15 +33,14 @@ public class ShakeEventListener extends Service implements SensorEventListener {
     private float mAccelCurrent = SensorManager.GRAVITY_EARTH;
     private float mAccelLast = SensorManager.GRAVITY_EARTH;
 
-//    private ShakeListener listener;
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        this.setLimit(intent.getExtras().getInt("limit"));
         System.out.println("servive start");
-        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI ,new Handler());
         return START_STICKY;
 
     }
@@ -55,6 +60,8 @@ public class ShakeEventListener extends Service implements SensorEventListener {
         mSensorManager.unregisterListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT_WATCH)
+    @Override
     public void onSensorChanged(SensorEvent se) {
         float x = se.values[0];
         float y = se.values[1];
@@ -64,13 +71,21 @@ public class ShakeEventListener extends Service implements SensorEventListener {
         float delta = mAccelCurrent - mAccelLast;
         mAccel = mAccel * 0.9f + delta;
         if(mAccel > SHAKE_LIMIT) {
-            System.out.println("shaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaake");
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-//                   setTurnScreenOn(true);
-//               } else {
-//                   final Window window = getWindow();
-//                   window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-//               }
+            System.out.println("shaaaake");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+                PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+                boolean isScreenOn = pm.isInteractive();
+                if (isScreenOn == false)
+                {
+                    PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, getPackageName()+"shake,s1");
+                    wl.acquire();
+                    wl.release();
+                }
+                else{
+                    Toast.makeText(getApplicationContext() , "sensitive = "+SHAKE_LIMIT , Toast.LENGTH_SHORT).show();
+
+                }
+            }
         }
     }
 
@@ -82,6 +97,11 @@ public class ShakeEventListener extends Service implements SensorEventListener {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mSensorManager.unregisterListener(this);
+
+        System.out.println("destroyyyyyyy");
+        Toast.makeText(getApplicationContext() , "destroy" , Toast.LENGTH_SHORT).show();
+
     }
 
 
